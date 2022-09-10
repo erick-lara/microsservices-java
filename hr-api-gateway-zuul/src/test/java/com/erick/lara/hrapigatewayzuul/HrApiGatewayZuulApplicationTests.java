a3f41613e385b08de7c3d3e01bd754d8f9934c76
Erick Lara <egmlara81@gmail.com>
2022-09-10 17:34:21 -0300

    feat: new project hr-api-gateway-zuul

---
+++ hr-api-gateway-zuul/src/test/java/com/erick/lara/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erick.lara.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
